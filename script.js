7df122bd29ad25c1c64f7b067ca531a19bdff3ec
Vkscoma <vkscoma@gmail.com>
2023-03-27 16:35:53 -0700

    last touches

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const userName = document.querySelectorAll('.user--name'),
-    userPic = document.querySelectorAll('.user--pic');
+    userPic = document.querySelectorAll('.user--pic'),
+    loggedInUser = document.querySelector('.logged--in__username'),
+    loggedInUserPic = document.querySelector('.logged--in__userpic');
+
+fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then((data) => {
+        const firstName = data.results[0].name.first
+        const lastName = data.results[0].name.last
+        const photo = data.results[0].picture.large
+
+        loggedInUserPic.src = photo
+        loggedInUser.textContent = `${firstName} ${lastName}`
+    });
 
 userName.forEach((user) => {
     fetch('https://randomuser.me/api/')
